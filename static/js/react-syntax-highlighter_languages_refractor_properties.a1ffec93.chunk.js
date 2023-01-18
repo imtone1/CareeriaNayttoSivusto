094f7f19ceea2df4586ff0ffb69bd5eb690a163a
imtone1 <88165529+imtone1@users.noreply.github.com>
2023-01-18 19:48:59 +0200

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_properties.a1ffec93.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknauttosovellus=self.webpackChunknauttosovellus||[]).push([[81],{9164:function(t){function s(t){t.languages.properties={comment:/^[ \t]*[#!].*$/m,"attr-value":{pattern:/(^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?: *[=:] *| ))(?:\\(?:\r\n|[\s\S])|[^\\\r\n])+/m,lookbehind:!0},"attr-name":/^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?= *[=:] *| )/m,punctuation:/[=:]/}}t.exports=s,s.displayName="properties",s.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_properties.a1ffec93.chunk.js.map
